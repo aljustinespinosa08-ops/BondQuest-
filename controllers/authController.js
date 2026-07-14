161dc5258014088684f702a895c3d04d3ebc1645
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 05:03:42 +0800

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,10 @@
 // Authentication controller
+middleware/auth.js
+routes/auth.js
+models/.gitkeep
+database/bondquest.sql
+public/css/style.css
+public/js/script.js
+views/login.html
+views/register.html
+views/dashboard.html
