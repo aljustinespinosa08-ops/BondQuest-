9617cc4ae0cc1ab773317905bae6b73e5869622b
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 04:49:55 +0800

    Create package.json

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "bondquest",
+  "version": "1.0.0",
+  "description": "Gamified Web Application for Ionic and Covalent Bonding",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "bcrypt": "^5.1.1",
+    "body-parser": "^1.20.2",
+    "chart.js": "^4.4.0",
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2",
+    "jsonwebtoken": "^9.0.2",
+    "multer": "^1.4.5-lts.1",
+    "mysql2": "^3.10.0"
+  }
+}
