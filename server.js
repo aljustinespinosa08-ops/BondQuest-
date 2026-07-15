4fd6ee8aa18b802c85b7477118b47771e4d70d7b
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 18:04:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
 const app = express();
 
+const authRoutes = require("./routes/auth");
+
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Home Route
 app.get("/", (req, res) => {
-    res.send("Welcome to BondQuest!");
+    res.send("🧪 BondQuest API is Running!");
 });
 
+// Server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`BondQuest Server running on port ${PORT}`);
 });
