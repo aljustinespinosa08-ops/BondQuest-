2db686a78a252b538747faae73b7d74c5cae0cd4
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 18:58:43 +0800

    Create public/js/pretest.js

---
+++ public/js/pretest.js
@@ -0,0 +1,11 @@
+let preTestScore = 0;
+
+function startPreTest() {
+    preTestScore = 0;
+    console.log("Pre-test Started");
+}
+
+function finishPreTest(score) {
+    preTestScore = score;
+    console.log("Pre-test Score:", preTestScore);
+}
