c7641b667cb423da46880799d64d6d2bc8d3cd14
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 17:54:46 +0800

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,41 @@
-// Authentication controller
-middleware/auth.js
-routes/auth.js
-models/.gitkeep
-database/bondquest.sql
-public/css/style.css
-public/js/script.js
-views/login.html
-views/register.html
-views/dashboard.html
+const db = require("../config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Register User
+exports.register = async (req, res) => {
+    try {
+        const { fullname, email, password, role } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const sql = `
+            INSERT INTO users (fullname, email, password, role)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        db.query(sql, [fullname, email, hashedPassword, role], (err, result) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            res.json({
+                success: true,
+                message: "Registration successful!"
+            });
+        });
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+// Login User
+exports.login = (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Login feature coming next."
+    });
+
+};
