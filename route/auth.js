3d68cb65f7a623b46a6e939708467cd152057faa
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 17:57:00 +0800

    Update auth.js

--- route/auth.js
+++ route/auth.js
@@ -1,1 +1,13 @@
-// Authentication routes
+
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+// Register
+router.post("/register", authController.register);
+
+// Login
+router.post("/login", authController.login);
+
+module.exports = router;
