46b62eb7c6b822e45a70811e0fbbfc2184f0cd1b
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 18:59:35 +0800

    Create public/js/posttest.js

---
+++ public/js/posttest.js
@@ -0,0 +1,11 @@
+let postTestScore = 0;
+
+function startPostTest() {
+    postTestScore = 0;
+    console.log("Post-test Started");
+}
+
+function finishPostTest(score) {
+    postTestScore = score;
+    console.log("Post-test Score:", postTestScore);
+}
