7aaf9b9f49fddda7e4d96ab38fb050db69a7560a
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-15 18:28:57 +0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,28 @@
-.gitkeep
+const questions = [
+    {
+        id: 1,
+        level: "Beginner",
+        question: "What type of bond is formed between Sodium (Na) and Chlorine (Cl)?",
+        choices: [
+            "Ionic Bond",
+            "Covalent Bond",
+            "Metallic Bond",
+            "Hydrogen Bond"
+        ],
+        answer: "Ionic Bond",
+        xp: 10,
+        coins: 5
+    }
+];
+
+let currentQuestion = 0;
+let score = 0;
+let xp = 0;
+let coins = 0;
+
+function startGame() {
+    console.log("BondQuest Started!");
+    console.log(questions[currentQuestion]);
+}
+
+startGame();
