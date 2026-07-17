227ac402cb3b0827db4c88fcaab32d03c21195b4
aljustinespinosa08-ops <aljustinespinosa08@gmail.com>
2026-07-17 10:20:26 +0800

    Update lives.js

--- public/js/lives.js
+++ public/js/lives.js
@@ -1,15 +1,25 @@
 let lives = 3;
 
 function loseLife() {
+
     lives--;
 
-    console.log("❤️ Lives Left:", lives);
+    document.getElementById("lives").textContent =
+        "❤️ Lives: " + lives;
 
     if (lives <= 0) {
+
         console.log("Game Over");
+
     }
+
 }
 
 function resetLives() {
+
     lives = 3;
+
+    document.getElementById("lives").textContent =
+        "❤️ Lives: " + lives;
+
 }
